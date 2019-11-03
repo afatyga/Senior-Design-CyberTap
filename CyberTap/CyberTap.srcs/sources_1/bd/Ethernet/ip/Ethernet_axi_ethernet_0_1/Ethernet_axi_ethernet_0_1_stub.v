4f97fc3362151dca5b482339ea511b4d566d3303
evanelang <evanlang@bu.edu>
2019-11-03 12:57:57 -0500

    Hardware Design for basic Microblaze Server
    Working Hardware design using microblaze sw microproccessor. USes ethernet. Currently requires SDK to get it running on the board.

---
+++ CyberTap/CyberTap.srcs/sources_1/bd/Ethernet/ip/Ethernet_axi_ethernet_0_1/Ethernet_axi_ethernet_0_1_stub.v
@@ -0,0 +1,90 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+// Date        : Sat Nov  2 13:56:56 2019
+// Host        : ECE-SD-T02 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub {x:/My
+//               Documents/SeniorDesign/CyberTap/CyberTap.srcs/sources_1/bd/Ethernet/ip/Ethernet_axi_ethernet_0_1/Ethernet_axi_ethernet_0_1_stub.v}
+// Design      : Ethernet_axi_ethernet_0_1
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tcsg324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "bd_db91,Vivado 2019.1" *)
+module Ethernet_axi_ethernet_0_1(s_axi_lite_resetn, s_axi_lite_clk, mac_irq, 
+  axis_clk, axi_txd_arstn, axi_txc_arstn, axi_rxd_arstn, axi_rxs_arstn, interrupt, gtx_clk, 
+  phy_rst_n, s_axi_araddr, s_axi_arready, s_axi_arvalid, s_axi_awaddr, s_axi_awready, 
+  s_axi_awvalid, s_axi_bready, s_axi_bresp, s_axi_bvalid, s_axi_rdata, s_axi_rready, 
+  s_axi_rresp, s_axi_rvalid, s_axi_wdata, s_axi_wready, s_axi_wstrb, s_axi_wvalid, 
+  s_axis_txc_tdata, s_axis_txc_tkeep, s_axis_txc_tlast, s_axis_txc_tready, 
+  s_axis_txc_tvalid, s_axis_txd_tdata, s_axis_txd_tkeep, s_axis_txd_tlast, 
+  s_axis_txd_tready, s_axis_txd_tvalid, m_axis_rxd_tdata, m_axis_rxd_tkeep, 
+  m_axis_rxd_tlast, m_axis_rxd_tready, m_axis_rxd_tvalid, m_axis_rxs_tdata, 
+  m_axis_rxs_tkeep, m_axis_rxs_tlast, m_axis_rxs_tready, m_axis_rxs_tvalid, mii_rx_clk, 
+  mii_rx_dv, mii_rx_er, mii_rxd, mii_tx_clk, mii_tx_en, mii_tx_er, mii_txd, mdio_mdc, mdio_mdio_i, 
+  mdio_mdio_o, mdio_mdio_t)
+/* synthesis syn_black_box black_box_pad_pin="s_axi_lite_resetn,s_axi_lite_clk,mac_irq,axis_clk,axi_txd_arstn,axi_txc_arstn,axi_rxd_arstn,axi_rxs_arstn,interrupt,gtx_clk,phy_rst_n[0:0],s_axi_araddr[17:0],s_axi_arready,s_axi_arvalid,s_axi_awaddr[17:0],s_axi_awready,s_axi_awvalid,s_axi_bready,s_axi_bresp[1:0],s_axi_bvalid,s_axi_rdata[31:0],s_axi_rready,s_axi_rresp[1:0],s_axi_rvalid,s_axi_wdata[31:0],s_axi_wready,s_axi_wstrb[3:0],s_axi_wvalid,s_axis_txc_tdata[31:0],s_axis_txc_tkeep[3:0],s_axis_txc_tlast,s_axis_txc_tready,s_axis_txc_tvalid,s_axis_txd_tdata[31:0],s_axis_txd_tkeep[3:0],s_axis_txd_tlast,s_axis_txd_tready,s_axis_txd_tvalid,m_axis_rxd_tdata[31:0],m_axis_rxd_tkeep[3:0],m_axis_rxd_tlast,m_axis_rxd_tready,m_axis_rxd_tvalid,m_axis_rxs_tdata[31:0],m_axis_rxs_tkeep[3:0],m_axis_rxs_tlast,m_axis_rxs_tready,m_axis_rxs_tvalid,mii_rx_clk,mii_rx_dv,mii_rx_er,mii_rxd[3:0],mii_tx_clk,mii_tx_en,mii_tx_er,mii_txd[3:0],mdio_mdc,mdio_mdio_i,mdio_mdio_o,mdio_mdio_t" */;
+  input s_axi_lite_resetn;
+  input s_axi_lite_clk;
+  output mac_irq;
+  input axis_clk;
+  input axi_txd_arstn;
+  input axi_txc_arstn;
+  input axi_rxd_arstn;
+  input axi_rxs_arstn;
+  output interrupt;
+  input gtx_clk;
+  output [0:0]phy_rst_n;
+  input [17:0]s_axi_araddr;
+  output s_axi_arready;
+  input s_axi_arvalid;
+  input [17:0]s_axi_awaddr;
+  output s_axi_awready;
+  input s_axi_awvalid;
+  input s_axi_bready;
+  output [1:0]s_axi_bresp;
+  output s_axi_bvalid;
+  output [31:0]s_axi_rdata;
+  input s_axi_rready;
+  output [1:0]s_axi_rresp;
+  output s_axi_rvalid;
+  input [31:0]s_axi_wdata;
+  output s_axi_wready;
+  input [3:0]s_axi_wstrb;
+  input s_axi_wvalid;
+  input [31:0]s_axis_txc_tdata;
+  input [3:0]s_axis_txc_tkeep;
+  input s_axis_txc_tlast;
+  output s_axis_txc_tready;
+  input s_axis_txc_tvalid;
+  input [31:0]s_axis_txd_tdata;
+  input [3:0]s_axis_txd_tkeep;
+  input s_axis_txd_tlast;
+  output s_axis_txd_tready;
+  input s_axis_txd_tvalid;
+  output [31:0]m_axis_rxd_tdata;
+  output [3:0]m_axis_rxd_tkeep;
+  output m_axis_rxd_tlast;
+  input m_axis_rxd_tready;
+  output m_axis_rxd_tvalid;
+  output [31:0]m_axis_rxs_tdata;
+  output [3:0]m_axis_rxs_tkeep;
+  output m_axis_rxs_tlast;
+  input m_axis_rxs_tready;
+  output m_axis_rxs_tvalid;
+  input mii_rx_clk;
+  input mii_rx_dv;
+  input mii_rx_er;
+  input [3:0]mii_rxd;
+  input mii_tx_clk;
+  output mii_tx_en;
+  output mii_tx_er;
+  output [3:0]mii_txd;
+  output mdio_mdc;
+  input mdio_mdio_i;
+  output mdio_mdio_o;
+  output mdio_mdio_t;
+endmodule
