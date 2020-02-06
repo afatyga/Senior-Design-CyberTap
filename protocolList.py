a9ad1a73bbddfa745f66981b62e315a29dc6a674
Alex <afatyga@bu.edu>
2020-02-06 10:27:56 -0500

    adding in a protocol list for parsing in the future - still a work in progress

---
+++ protocolList.py
@@ -0,0 +1,29 @@
+protocols = {
+	"TCP" : {
+		"source" : 16,
+		"desination" : 16,
+		"seqNum" : 32,
+		"acknowledge" : 32,
+		"offset" : 4,
+		"reserved" : 3,
+		"flags" : 9,
+		"window" : 16,
+		"checksum" : 16,
+		"urgentPtr" : 16,
+		"options" : "variable"
+
+	},
+	"ModBus" : {
+		
+
+	},
+	"DNP3" : {
+
+	},
+	"IP" : {
+
+	},
+	"ARP" : {
+
+	}
+}
